252350eac1769d3215f7e518d4a090d3e499c158
ShunQ04 <113802850+Kaisha04@users.noreply.github.com>
2026-07-27 17:47:59 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+    Створіть програму, яка виводитиме на екран ланцюжки падаючих символів. Довжина кожного ланцюжка визначається випадково. Перший символ ланцюжка – білий, другий символ – світло-зелений, решта символів темно-зелені. 
+    Під час падіння ланцюжка на кожному кроці всі символи змінюють своє значення.
+    Дійшовши до кінця, ланцюжок зникає і зверху формується новий ланцюжок. Дивіться example1.png представлений як зразок.*/
+
+
+using System;
+
+namespace Task1;
+
+class Program
+{
+    static object locker = new();
+    public static void SpawnSymbols()
+    {
+        Random r = new Random();
+        Console.SetCursorPosition(r.Next(1, 20), 0);
+        while (true)
+        {
+            int symbol = r.Next(32, 112);
+            if (symbol % 2 == 0)
+            {
+                Console.WriteLine();
+            }else Console.WriteLine((char)symbol);
+        }
+    }
+
+    public static void GetColor()
+    {
+        switch(Random.Shared.Next(1,5))
+        {
+            case 1: Console.ForegroundColor = ConsoleColor.Red;
+                break;
+            case 2: Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case 3: Console.ForegroundColor = ConsoleColor.Blue;
+                break;
+            case 4:
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                break;
+            case 5:
+                Console.ForegroundColor = ConsoleColor.Green;
+                break;
+        }
+    }
+    static void Main()
+    {
+        Thread firstThread = new Thread(SpawnSymbols);
+        Thread secondThread = new Thread(SpawnSymbols);
+        Thread thirdThread = new Thread(SpawnSymbols);
+
+        firstThread.Start();
+        secondThread.Start();
+        thirdThread.Start();
+    }
+}
